f550edeb2d315141ecd72808fd386af68cb1dbff
TJM-HZ <mein0009@hz.nl>
2022-02-10 10:10:31 +0100

    Re-introduced the heads for now, deleted head.php

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
 <html lang="en-us">
 
-@include("./generic/head")
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tim's Website | FAQ</title>
+    <meta name="description"
+          content="This is the FAQ, where you'll find some answers for frequently asked questions at the HZ.">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="bs5/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/faq.css">
+    <link rel="canonical" href="https://tjm-hz.github.io/faq">
+
+</head>
 
 <body>
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
 <html lang="en-us">
 
-@include("./generic/head")
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tim's Website | Profile</title>
+    <meta name="description"
+          content="Here you'll find a bit of information about me. Who I am, what I like, what I do, that sort of thing.">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="bs5/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/profile.css">
+    <link rel="canonical" href="https://tjm-hz.github.io/profile">
+</head>
+
 
 <body>
 
