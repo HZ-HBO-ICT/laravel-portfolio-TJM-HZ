9eed924ad3e1eb1311d8f21d9182a1cd5544466d
TJM-HZ <mein0009@hz.nl>
2022-02-13 21:58:51 +0100

    Finished week 1

---
+++ app/Models/Grade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    //use HasFactory;
+
+    public function addResult($grade) {
+        if($grade > $this->best_grade) {
+            $this->best_grade = $grade;
+        }
+
+        if($grade >= $this->lowest_passing_grade) {
+            $this->passed_at = now();
+        }
+
+        $this->save();
+    }
+}
